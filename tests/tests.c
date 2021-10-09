a737f5137d0300fed549d5f13f7605f388cd7639
Alexandre GAUTIER <alexandre.gautier@epita.fr>
2021-10-09 10:47:54 +0200

    add operation priorities tests

--- tests/tests.c
+++ tests/tests.c
@@ -25,3 +25,22 @@ Test(simple, mul)
     int res = evalexpr("3*4");
     cr_assert_eq(12, res, "EXPECTED: %d, GOT: %d", 12, res);
 }
+
+Test(two_op, add_mul)
+{
+    int res = evalexpr("3+4*2");
+    cr_assert_eq(11, res, "EXPECTED: %d, GOT: %d", 11, res);
+}
+
+Test(two_op, mul_sub)
+{
+    int res = evalexpr("3*4-2");
+    cr_assert_eq(10, res, "EXPECTED: %d, GOT: %d", 10, res);
+}
+
+Test(two_op, add_sub)
+{
+    int res = evalexpr("3+4-2");
+    cr_assert_eq(6, res, "EXPECTED: %d, GOT: %d", 6, res);
+}
+
